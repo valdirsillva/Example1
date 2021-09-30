7bb8a0c4e91e194e0bec616be344e5641195aeef
Valdir Silva <valdirpiresba@gmail.com>
2021-09-30 01:02:24 -0300

    Escrevendo a classe de produtos

---
+++ src/LIVRO/Loja/Produto/Produto.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace LIVRO\Loja\Carrinho;
+
+
+class Produto 
+{
+    private $nome;
+    private $valor;
+
+    public function __construct($nome, $valor) 
+    {
+        $this->nome = $nome;
+        $this->valor = $valor;
+    }
+
+    function getNome() 
+    {
+        return $this->nome;
+    }
+
+    function getValor() 
+    {
+        return $this->valor;
+    }
+}
+
