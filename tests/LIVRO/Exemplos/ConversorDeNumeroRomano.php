af0898f7a2a3ace63f83041da144aa649e7be58a
Valdir Silva <valdirpiresba@gmail.com>
2021-09-30 14:42:58 -0300

    Rodando o teste que falhara da primeira vez

--- tests/LIVRO/Exemplos/ConversorDeNumeroRomano.php
+++ tests/LIVRO/Exemplos/ConversorDeNumeroRomano.php
@@ -8,13 +8,13 @@ require "./vendor/autoload.php";
 use LIVRO\Exemplos\ConversorDeNumeroRomano;
 use PHPUnit_Framework_TestCase as PHPUnit;
 
-class ConversorDeNumeroRomano extends PHPUnit 
+class ConversorDeNumeroRomanoTest extends PHPUnit 
 {
    
     public function testDeveEntenderOSimboloI() 
     {
         
-        $romano = new ConvesorDeNumeroRomano();
+        $romano = new ConversorDeNumeroRomano();
         $numero = $romano->converte("I");
         $this->assertEquals(1, $numero); 
     }
