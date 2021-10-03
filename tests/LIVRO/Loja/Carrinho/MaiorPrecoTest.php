9870e15a1128a768fd724305094ca9aafc08b178
Valdir Silva <valdirpiresba@gmail.com>
2021-10-03 15:04:17 -0300

    Corrigindo tests da classe carrinho

--- tests/LIVRO/Loja/Carrinho/MaiorPrecoTest.php
+++ tests/LIVRO/Loja/Carrinho/MaiorPrecoTest.php
@@ -29,8 +29,10 @@ class MaiorPrecoTest extends TestCase
         $carrinho = new CarrinhoDeCompras();
         $carrinho->adiciona(new Produto("Geladeira", 1, 900.00));
 
-        $maiorPreco = new MaiorPreco();
-        $valor = $maiorPreco->encontra($carrinho);        
+        // $maiorPreco = new MaiorPreco();
+        // $valor = $maiorPreco->encontra($carrinho);
+        
+        $valor = $carrinho->maiorValor();
 
         $this->assertEquals(900.00, $valor, null, 0.0001);
     }
@@ -40,12 +42,14 @@ class MaiorPrecoTest extends TestCase
         $carrinho = new CarrinhoDeCompras();
 
         $carrinho->adiciona( new Produto("Geladeira", 1, 900.00));
-        $carrinho->adiciona( new Produto("Figão", 1, 1500.00));
+        $carrinho->adiciona( new Produto("Fogão", 1, 1500.00));
         $carrinho->adiciona( new Produto("Máquina de lavar", 1, 750.00));
 
-        $maiorPreco = new MaiorPreco();
-        $Valor = $maiorPreco->encontra($carrinho);
+        // $maiorPreco = new MaiorPreco();
+        // $Valor = $maiorPreco->encontra($carrinho);
 
-        $this->assertEquals(1500.0, $valor, null, 0.0001);
+        $valor = $carrinho->maiorValor();
+
+        $this->assertEquals(1500.00, $valor, null, 0.0001);
     }
 }
